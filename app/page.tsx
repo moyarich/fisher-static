50ba9db4618d4629d0ec7d8961c8810002059cdb
moyarich <moyarich@hotmail.com>
2023-11-02 15:43:31 -0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main className="container flex h-screen flex-col p-6">
+      <div className="md-auto">hi Micheal</div>
+    </main>
+  );
+}
